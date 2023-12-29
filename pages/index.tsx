827d6373a493fb545d75200c954f13b038b42e73
seojiho <happyluck0426@gamil.com>
2023-12-29 15:40:50 +0900

    폴더구조

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <Theme>
+    
+    </Theme>
+  )
+}
+
+export default Home
